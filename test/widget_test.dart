920a287f19e1512276f2554e64a35b0ade470cc3
rizona27 <rizona.cn@gmail.com>
2026-05-03 22:33:44 +0800

    Delete widget_test.dart

--- test/widget_test.dart
+++
@@ -1,9 +0,0 @@
-// This is a basic Flutter widget test.
-import 'package:flutter_test/flutter_test.dart';
-
-void main() {
-  testWidgets('Placeholder test', (WidgetTester tester) async {
-    // 由于没有实际的应用入口，这里只验证测试框架能运行
-    expect(true, true);
-  });
-}
